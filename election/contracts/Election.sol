855dd748bcae2f6aa4653288227a97b3685b7646
monicaar <rameshbabu9497@gmail.com>
2019-01-14 22:05:37 +0530

    first transaction

---
+++ election/contracts/Election.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+
+/**
+ * The contractName contract does this and that...
+ */
+contract Election {
+	//constructor
+	string public candidate;
+	function Elections() public{
+		candidate="candidate 1";
+
+	}	
+}
+

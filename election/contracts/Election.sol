4b729cb9c1dd796a73b2ce417f0de8a3aa19f096
monicaar <rameshbabu9497@gmail.com>
2019-01-15 13:24:34 +0530

    adding candidates

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -6,10 +6,25 @@ pragma solidity ^0.5.0;
  */
 contract Election {
 	//constructor
-	string public candidate;
-	function Elections() public{
-		candidate="candidate 1";
+	struct Candidate{
+		uint id;//unsigned integer
+		string name;
+		uint voteCount;
+	}
 
+	mapping(uint => Candidate) public candidates;
+
+
+	uint public candidatesCount;
+	function Elections() public{
+		addCandidate("candidate 1");
+		addCandidate("candidate 2");
+		}
+
+
+	function addCandidate (string memory _name) internal {//local variable name
+		candidatesCount ++;
+	 candidates[candidatesCount] = Candidate(candidatesCount, _name , 0);
 	}	
 }
 
